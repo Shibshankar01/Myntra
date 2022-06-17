d3177da69fc71fff3995a2f310193e389276bd66
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-06-17 14:37:33 +0530

    adding functionality

--- bag.js
+++ bag.js
@@ -28,36 +28,61 @@ sizeTag.addEventListener("click", () => {
    sizeModal.style.display = "block";
 });
 
+//Covid 19 modal
+let covidKnowMoreTag = document.querySelector(".donationStrip-KnowMore");
+let covidKnowMoreModel = document.querySelector("#knoworemodal");
+
+covidKnowMoreTag.addEventListener("click", () => {
+   covidKnowMoreModel.style.display = "block";
+});
+
+//COUPONS MODAL
+let couponsApplyTag = document.querySelector(".coupons-apply");
+let couponsModal = document.querySelector("#couponsmodal");
+
+couponsApplyTag.addEventListener("click", () => {
+   couponsModal.style.display = "block";
+});
+
 //modal closing
-let closeModel = document.querySelectorAll(".modal-cancelIcon");
-closeModel.forEach((el) => {
+let closeModal = document.querySelectorAll(".modal-cancelIcon");
+closeModal.forEach((el) => {
    el.addEventListener("click", (event) => {
-      if (event.target.parentNode.parentNode == "qtymodal") {
-         qtymodal.style.display = "none";
-      }
-      if (event.target.parentNode.parentNode.id == "sizemodal") {
-         sizemodal.style.display = "none";
+      if (event.target.parentNode.parentNode.id == "qtymodal") {
+         qtyModal.style.display = "none";
+      } else if (event.target.parentNode.parentNode.id == "sizemodal") {
+         sizeModal.style.display = "none";
+      } else if (event.target.parentNode.parentNode.id == "knoworemodal") {
+         covidKnowMoreModel.style.display = "none";
+      } else if (event.target.parentNode.parentNode.id == "couponsmodal") {
+         couponsModal.style.display = "none";
       }
    });
 });
 
 window.addEventListener("click", (event) => {
    if (event.target.id == "qtymodal") {
-      qtymodal.style.display = "none";
+      qtyModal.style.display = "none";
    }
    if (event.target.id == "sizemodal") {
-      sizemodal.style.display = "none";
+      sizeModal.style.display = "none";
+   }
+   if (event.target.id == "knoworemodal") {
+      covidKnowMoreModel.style.display = "none";
+   }
+   if (event.target.id == "couponsmodal") {
+      couponsModal.style.display = "none";
    }
 });
 
 //covid donation
 let covidDon = 0;
 
-let donPill = document.querySelectorAll(".pillView-textStyle");
+let donPill = document.querySelectorAll(".pillView-pill");
 
 donPill.forEach((el) => {
    el.addEventListener("click", (event) => {
-      covidDon = +event.target.parentNode.getAttribute("data-key");
+      covidDon = +event.target.getAttribute("data-key");
       document
          .querySelector(".donationStrip-icon > path")
          .setAttribute(
@@ -66,6 +91,7 @@ donPill.forEach((el) => {
          );
    });
 });
+
 document.querySelector(".donationStrip-input").addEventListener("change", (event) => {
    covidDon = +document.querySelector(".donationStrip-input").value;
    document
@@ -99,3 +125,80 @@ document.querySelector(".donationStrip-icon").addEventListener("click", (event) 
       covidDon = 0;
    }
 });
+
+//itemContainer-item
+
+displayItems();
+
+function displayItems() {
+   let cartItemData = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+   let itemContainer = document.querySelector(".itemContainer");
+   itemContainer.innerText = "";
+
+   cartItemData.forEach((el, ind) => {
+      let Item = document.createElement("div");
+      Item.setAttribute("class", "itemContainer-item");
+      let ItemLeft = document.createElement("div");
+      ItemLeft.setAttribute("class", "itemContainer-itemLeft");
+      let itemImg = document.createElement("img");
+
+      itemImg.setAttribute("src", el.images.split(" | ")[0]);
+      ItemLeft.append(itemImg);
+
+      let ItemRight = document.createElement("div");
+      ItemRight.setAttribute("class", "itemContainer-itemRight");
+
+      let itemBrand = document.createElement("h5");
+      itemBrand.innerText = el.brand;
+      itemBrand.setAttribute("class", "itemContainer-brand");
+      let itemTitle = document.createElement("p");
+      itemTitle.innerText = el.title;
+      itemTitle.setAttribute("class", "itemContainer-title");
+      let itemSeller = document.createElement("p");
+      itemSeller.innerText = "Sold By: Omnitech Retail";
+      itemSeller.setAttribute("class", "itemContainer-soldby");
+
+      let itemSizeQtyContainer = document.createElement("div");
+      itemSizeQtyContainer.setAttribute("class", "sizeQtyContainer");
+      let itemSize = document.createElement("div");
+      itemSize.innerText = "Size: " + el.size;
+      itemSize.setAttribute("class", "item-size");
+      let itemQty = document.createElement("div");
+      itemQty.innerText = "Qty: " + el.qty;
+      itemQty.setAttribute("class", "item-qty");
+      itemSizeQtyContainer.append(itemSize, itemQty);
+
+      let itemPriceContainer = document.createElement("div");
+      itemPriceContainer.setAttribute("class", "itemPriceContainer");
+      let itemPrice = document.createElement("h5");
+      itemPrice.innerText = "₹" + el.variant_price;
+      itemPrice.setAttribute("class", "item-price");
+      let ogPrice = document.createElement("h5");
+      ogPrice.innerText = "₹" + el.variant_compare_at_price;
+      ogPrice.setAttribute("class", "og-price");
+      let offPrice = document.createElement("h5");
+      let off = 100 - ((el.variant_price / el.variant_compare_at_price) * 100).toFixed(0);
+      offPrice.innerText = "(" + off + "% OFF)";
+      offPrice.setAttribute("class", "orange-1");
+      itemPriceContainer.append(itemPrice, ogPrice, offPrice);
+
+      let deliveryContainer = document.createElement("div");
+      deliveryContainer.setAttribute("class", "deliveryContainer");
+      let deliveryIcon = document.createElement("span");
+      deliveryIcon.setAttribute("class", "myntraweb-sprite sprites-greenTickIcon");
+      let deliveryText = document.createElement("span");
+      deliveryText.innerText = "Delivery By";
+      let deliveryDate = document.createElement("strong");
+
+      let today = new Date();
+
+      deliveryDate.innerText = +(today.getDate() + 3) + " " + today.toString().substring(4, 7) + " " + today.getFullYear();
+      deliveryContainer.append(deliveryIcon, deliveryText, deliveryDate);
+
+      ItemRight.append(itemBrand, itemTitle, itemSeller, itemSizeQtyContainer, itemPriceContainer, deliveryContainer);
+      Item.append(ItemLeft, ItemRight);
+
+      itemContainer.append(Item);
+   });
+}
