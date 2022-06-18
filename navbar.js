747dae44b14ef59be456c4a7030ad1345fa2752d
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-06-18 21:07:07 +0530

    updated Navbar.js

--- navbar.js
+++ navbar.js
@@ -1,22 +1,4 @@
-document.querySelector("#btn").addEventListener("click", login);
-function login() {
-   window.location.href = "login or  signup.html";
-}
-let menDiv = document.querySelector("#men");
-menDiv.addEventListener("click", menClick);
-let womenDiv = document.querySelector("#women");
-womenDiv.addEventListener("click", womenClick);
-let kidsDiv = document.querySelector("#kids");
-kidsDiv.addEventListener("click", kidsClick);
-function menClick() {
-   localStorage.setItem("clickedOn", "Men");
-   window.location.href = "productsPage.html";
-}
-function womenClick() {
-   localStorage.setItem("clickedOn", "Women");
-   window.location.href = "productsPage.html";
-}
-function kidsClick() {
-   localStorage.setItem("clickedOn", "Kids");
-   window.location.href = "productsPage.html";
+document.querySelector("#btn").addEventListener("click",login)
+function login(){
+    window.location.href="login or  signup.html"
 }
