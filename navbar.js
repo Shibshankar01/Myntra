1eeb1ca24eb9dce0fab80a2d2cb0e25dd497894f
Shibshankar Padhy <shivapadhy547@gmail.com>
2022-06-18 20:32:59 +0530

    Test

--- navbar.js
+++ navbar.js
@@ -1,22 +1,4 @@
-let bag = document.querySelector(".bag");
-bag.addEventListener("click", function () {
-   window.location.href = "bag.html";
-});
-let menDiv = document.querySelector("#men");
-menDiv.addEventListener("click", menClick);
-let womenDiv = document.querySelector("#women");
-womenDiv.addEventListener("click", womenClick);
-let kidsDiv = document.querySelector("#kids");
-kidsDiv.addEventListener("click", kidsClick);
-function menClick(){
-   localStorage.setItem("clickedOn", "Men");
-   window.location.href = "productsPage.html";
-}
-function womenClick(){
-   localStorage.setItem("clickedOn", "Women");
-   window.location.href = "productsPage.html";
-}
-function kidsClick() {
-   localStorage.setItem("clickedOn", "Kids");
-   window.location.href = "productsPage.html"
+document.querySelector("#btn").addEventListener("click",login)
+function login(){
+    window.location.href="login or  signup.html"
 }
