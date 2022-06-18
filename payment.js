802e78a6f8ae1b2ad6af515514173d141a9f14ec
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-06-18 18:13:46 +0530

    calculating amt

--- payment.js
+++ payment.js
@@ -1,1 +1,26 @@
+//update priceBlock-container
 
+updatePriceblock();
+function updatePriceblock() {
+   let orderData = JSON.parse(localStorage.getItem("orderData")) || [];
+
+   let orderSummary = document.querySelector("priceBreakUp-orderSummary");
+   document.querySelector(".actual-price").innerText = "₹" + orderData[0].original_amt;
+   document.querySelector(".discount-price").innerText = "-₹" + (orderData[0].original_amt - orderData[0].actual_amt);
+   let couponAmt = orderData[0].coupon_amt.toFixed(2);
+   if (couponAmt !== 0) {
+      document.querySelector(".applyCoupon").parentNode.style.display = "flex";
+      document.querySelector(".applyCoupon").innerText = "-₹" + orderData[0].coupon_amt.toFixed(2);
+   } else {
+      document.querySelector(".applyCoupon").parentNode.style.display = "none";
+   }
+
+   let covidDon = orderData[0].coviddon_amt.toFixed(2);
+   if (covidDon !== 0) {
+      document.querySelector(".donation-price").parentNode.style.display = "flex";
+      document.querySelector(".donation-price").innerText = "₹" + covidDon;
+   } else {
+      document.querySelector(".donation-price").parentNode.style.display = "none";
+   }
+   document.querySelector(".priceDetail-total").innerText = "₹" + orderData[0].order_amt;
+}
